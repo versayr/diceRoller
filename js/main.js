90b674e72308e3ebb5d786527d2fae3a73286bf1
Versayr <themoose3@MacBook-Pro.local>
2016-02-26 11:55:47 -0500

    Added an array that saves a list of the results of each roll

--- js/main.js
+++ js/main.js
@@ -1,16 +1,26 @@
 var numberSides = 6;
 var quantityDice = 3;
+var resultsList = [];
+var total = 0;
 
 function rollDice(diceSides, quantityDice) {
-  var total = 0;
+  resultsList = [];
+  total = 0;
   for (var i = 0; i < quantityDice; i++) {
     console.log(i);
     var rollResult = 1 + Math.floor(Math.random() * diceSides);
     console.log("Roll Result = " + rollResult);
     total = total + rollResult;
     console.log("Total = " + total);
+    resultsList.push(rollResult);
+    console.log(resultsList);
   };
-  $('#total').text(total);
+  // $('#total').text(total);
+};
+
+function printResults(resultsList, total) {
+  $('#resultsField').prepend('<br><span>= ' + total + '</span>');
+  $('#resultsField').prepend('<span>' + resultsList + '</span>');
 };
 
 $(document).ready(function() {
@@ -18,5 +28,6 @@ $(document).ready(function() {
     quantityDice = $('#quantityDice').val();
     numberSides = $('#numberSides').val();
     rollDice(numberSides, quantityDice);
+    printResults(resultsList, total);
   });
 });
